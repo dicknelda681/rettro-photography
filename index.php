fe905ddfb6ccbf33db70ddb65d39ff73e2516b2a
dicknelda681 <dicknelda681@gmail.com>
2026-01-14 15:59:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,19 @@
       <link rel="stylesheet" href="assets/css/style.css">
    </head>
    <body>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69676faf7c9eed197f686aad/1jeu0ohq7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
       <!-- ? Preloader Start -->
       <div id="preloader-active">
          <div class="preloader d-flex align-items-center justify-content-center">
